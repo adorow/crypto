8dabb25de8454ed0e6cd55ad344b6cb020279295
Anderson Dorow <andersondorow@gmail.com>
2016-03-23 13:54:08 -0700

    added the vignere cipher

--- haskell/src/Cipher/Substitution.hs
+++ haskell/src/Cipher/Substitution.hs
@@ -16,3 +16,6 @@ module Cipher.Substitution (
   substitutionCipherIgnoreMissing :: (Eq a) => (a -> Maybe a) -> [a] -> [a]
   substitutionCipherIgnoreMissing lookupFunction text =
     substitutionCipherKeepMissing lookupFunction $ filter (isJust . lookupFunction) text
+
+  vignereCipher :: (Eq a) => [(a -> a)] -> [a] -> [a]
+  vignereCipher lookupFunctions text = zipWith (\lookupFunction -> \char -> lookupFunction char) (take (length text) . cycle $ lookupFunctions) text
